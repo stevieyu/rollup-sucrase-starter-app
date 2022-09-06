0e6e40c1761f52dadfd90d2729d0807b3f573b67
StevieYu <yzm629@sina.cn>
2022-09-06 07:50:07 +0000

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,37 @@
 import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import sucrase from '@rollup/plugin-sucrase';
 
-// `npm run build` -> `production` is true
-// `npm run dev` -> `production` is false
-const production = !process.env.ROLLUP_WATCH;
+// `npm run build` -> `isProd` is true
+// `npm run dev` -> `isProd` is false
+const isProd = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.jsx',
+	input: 'src/main.js',
 	output: {
 		file: 'public/bundle.js',
 		format: 'es', // immediately-invoked function expression — suitable for <script> tags
 		sourcemap: true
 	},
 	plugins: [
+		replace({
+			values: {
+				'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development'),
+			},
+			preventAssignment: true,
+		}),
 		resolve({
-			extensions: ['.js','.jsx', '.ts', '.ts']
+			extensions: ['.js','.jsx', '.ts', '.tsx']
 		}), // tells Rollup how to find date-fns in node_modules
 		commonjs({
-			include: ["node_modules/**"],
-		}), // converts date-fns to ES modules
-		sucrase({
-			//exclude: ['node_modules/**'],
-			transforms: ['typescript', 'imports', 'jsx', 'react-hot-loader']
+			include: /node_modules/,
 		}),
-		production && terser() // minify, but only in production
-	],
-	context: "window"
+		sucrase({
+			exclude: ['node_modules/**'],
+			transforms: ['typescript', 'jsx', 'react-hot-loader']
+		}),
+		isProd && terser() // minify, but only in production
+	]
 };
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+console.log('React', React)
+
+
+// const App = () => {
+//     return (
+//         <div>hello word</div>
+//     )
+// }
+
+// const el = document.createElement('div')
+// document.body.appendChild(el)
+
+// ReactDOM.render(<App />, el);
