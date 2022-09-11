cc4858de125cfa047c74d55ced2fadc8e09c41b6
StevieYu <stevieyu@users.noreply.github.com>
2022-09-12 00:12:59 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
-//import commonjs from '@rollup/plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import sucrase from '@rollup/plugin-sucrase';
 
@@ -15,6 +15,7 @@ export default {
 		sourcemap: true
 	},
 	plugins: [
+		commonjs(), // converts date-fns to ES modules
 		resolve({
 			extensions: ['.js','.jsx', '.ts', '.tsx']
 		}), // tells Rollup how to find date-fns in node_modules
@@ -22,7 +23,6 @@ export default {
 			exclude: ['node_modules/**'],
 			transforms: ['typescript']
 		}),
-		//commonjs(), // converts date-fns to ES modules
 		production && terser() // minify, but only in production
 	]
 };
