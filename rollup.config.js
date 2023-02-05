ed6a5ea917ae639a94911f1349eeaf45e2a729bd
Stevie <yzm629@sina.cn>
2023-02-05 19:27:33 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import sucrase from '@rollup/plugin-sucrase';
+import html from '@rollup/plugin-html'
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -10,19 +11,25 @@ const production = !process.env.ROLLUP_WATCH;
 export default {
 	input: 'src/main.js',
 	output: {
-		file: 'public/bundle.js',
-		format: 'es', // immediately-invoked function expression — suitable for <script> tags
-		sourcemap: true
+		entryFileNames: 'bundle/main.[hash].js',
+		chunkFileNames: 'bundle/chunk.[hash].[ext]',
+		format: 'esm', // immediately-invoked function expression — suitable for <script> tags
+		sourcemap: true,
+		dir: 'dist'
 	},
+	external: [
+		'https://openfpcdn.io/fingerprintjs/v3'
+	],
 	plugins: [
 		commonjs(), // converts date-fns to ES modules
-		resolve({
+        nodeResolve({
 			extensions: ['.js','.jsx', '.ts', '.tsx']
 		}), // tells Rollup how to find date-fns in node_modules
 		sucrase({
 			exclude: ['node_modules/**'],
-			transforms: ['typescript']
+			transforms: ['typescript', 'jsx']
 		}),
-		production && terser() // minify, but only in production
+		production && terser(), // minify, but only in production
+		html(),
 	]
 };
